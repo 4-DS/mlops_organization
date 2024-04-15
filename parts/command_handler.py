a7ab469907791ed6ee4669161fa7c3a877011ccc
ExcaliburKG <gorbunov.kirill@gmail.com>
2024-04-15 15:54:01 +0300

    Added sinara volume list command

--- parts/command_handler.py
+++ parts/command_handler.py
@@ -1,5 +1,6 @@
 from .server import SinaraServer
 from .model import SinaraModel
+from .volume import SinaraVolume
 
 class CommandHandler:
 
@@ -8,3 +9,4 @@ class CommandHandler:
         print('This is ml_ops_organization')
         SinaraServer.add_command_handlers(root_parser, subject_parser)
         SinaraModel.add_command_handlers(root_parser, subject_parser)
+        SinaraVolume.add_command_handlers(root_parser, subject_parser)
