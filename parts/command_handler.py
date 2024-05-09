46f0e37882b9a6187dab8fc8784ba66497a090f3
pestovid <ilya.pestov@outlook.com>
2024-05-09 10:31:33 +0300

    changed message

--- parts/command_handler.py
+++ parts/command_handler.py
@@ -6,7 +6,7 @@ class CommandHandler:
 
     @staticmethod
     def add_command_handlers(root_parser, subject_parser):
-        #print('This is ml_ops_organization')
+        print('ml_ops_organization: personal')
         SinaraServer.add_command_handlers(root_parser, subject_parser)
         SinaraModel.add_command_handlers(root_parser, subject_parser)
         SinaraVolume.add_command_handlers(root_parser, subject_parser)
