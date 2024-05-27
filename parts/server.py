e04e1080b8e41a0d5d046197d9aac32151fe1288
pestovid <ilya.pestov@outlook.com>
2024-05-27 18:09:14 +0300

    added useCustomFolders parameter

--- parts/server.py
+++ parts/server.py
@@ -63,7 +63,8 @@ class SinaraServer():
         SinaraServer.create_parser = server_cmd_parser.add_parser('create', help='create sinara server')
         SinaraServer.create_parser.add_argument('--instanceName', default=SinaraServer.container_name, type=str, help='sinara server container name (default: %(default)s)')
         SinaraServer.create_parser.add_argument('--runMode', default='q', choices=["q", "b"], help='Runmode, quick (q) - work, data, tmp will be mounted inside docker volumes, basic (b) - work, data, tmp will be mounted from host folders (default: %(default)s)')
-        SinaraServer.create_parser.add_argument('--createFolders', action='store_false', help='create work, data, tmp folders in basic mode automatically if not exists, or else folders must be created manually (default: %(default)s)')
+        SinaraServer.create_parser.add_argument('--createFolders', action='store_false', help='Create work, data, tmp folders in basic mode automatically if not exists, or else folders must be created manually (default: %(default)s)')
+        SinaraServer.create_parser.add_argument('--useCustomFolders', action='store_true', help='Use custom work, data, raw and tmp folders in basic mode. Folders must exist (default: %(default)s)')
         SinaraServer.create_parser.add_argument('--gpuEnabled', choices=["y", "n"], help='y - Enables docker container to use Nvidia GPU, n - disable GPU')
         SinaraServer.create_parser.add_argument('--memLimit', default=str(SinaraServer.get_memory_size_limit()), type=str, help='Maximum amount of memory for server container (default: %(default)s)')
         SinaraServer.create_parser.add_argument('--cpuLimit', default=SinaraServer.get_cpu_cores_limit(), type=int, help='Number of CPU cores to use for server container (default: %(default)s)')
@@ -323,7 +324,7 @@ class SinaraServer():
     def _prepare_basic_mode(args):
         #folders_exist = ''
         
-        if args.createFolders == True or args.createFolders == "y":
+        if args.useCustomFolders != True:
              
             if args.jovyanRootPath:
                 jovyan_root_path = get_expanded_path(args.jovyanRootPath)
