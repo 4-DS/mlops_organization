9c781ab54d706e929de484b960af443a46c6c889
gorbunovka <ka.gorbunov@outlook.com>
2025-04-16 09:50:40 +0300

    fixed platform

--- parts/sinara_platform.py
+++ parts/sinara_platform.py
@@ -3,7 +3,7 @@ from enum import Enum
 class SinaraPlatform(Enum):
     #Desktop = 'desktop'
     #RemoteVM = 'remote'
-    #Personal = 'personal'
+    Personal = 'personal'
     PersonalPublicDesktop = "personal_public_desktop"
 
     def __str__(self):
